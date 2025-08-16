428af49fbcb7de14a17a79da3d5173f160a0113a
mbuiop <milad.yari1377m@gmail.com>
2025-08-16 16:34:05 +0330

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,5 @@
+// اسکریپت‌های کلی
+document.addEventListener('DOMContentLoaded', function() {
+    // می‌توانید اسکریپت‌های مورد نیاز خود را اینجا اضافه کنید
+    console.log('صفحه بارگذاری شد');
+});
