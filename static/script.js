a0d06e22f482efb1ef50be3f7ac5f3ef666f0263
mbuiop <milad.yari1377m@gmail.com>
2025-08-16 16:51:17 +0330

    Delete static directory

--- static/script.js
+++
@@ -1,5 +0,0 @@
-// اسکریپت‌های کلی
-document.addEventListener('DOMContentLoaded', function() {
-    // می‌توانید اسکریپت‌های مورد نیاز خود را اینجا اضافه کنید
-    console.log('صفحه بارگذاری شد');
-});
